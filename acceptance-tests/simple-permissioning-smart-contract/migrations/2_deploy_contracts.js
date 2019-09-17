b9aec8ba34bb35994aaa28928ad89959acfb390a
Edward <edward.evans@consensys.net>
2019-09-17 13:33:33 +1000

    Final Notice before Archive (#1947)
    Signed-off-by: Edward Evans <edward.evans@consensys.net>

--- acceptance-tests/simple-permissioning-smart-contract/migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-var SimpleNodePermissioning = artifacts.require("SimpleNodePermissioning");
-var SimpleAccountPermissioning = artifacts.require("SimpleAccountPermissioning");
-
-module.exports = function(deployer) {
-  deployer.deploy(SimpleNodePermissioning);
-  deployer.deploy(SimpleAccountPermissioning);
-};
--- docs/README.md
+++
@@ -1,5 +0,0 @@
-Besu user documentation was moved to as separate repository to help manage versions and releases.
-
-If you want to contribute to the doc site, make a pull request against https://github.com/hyperledger/besu-docs
-
-The generated doc website is at https://besu.hyperledger.org/
